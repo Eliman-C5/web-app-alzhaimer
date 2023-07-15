1556036fc2fe1d89cccc98358ab4ac079cb61b07
eli castellano <eliman1926@gmail.com>
2023-07-14 22:23:23 -0400

    Agregando iframe de guia de usuario

--- src/app/guia/page.tsx
+++ src/app/guia/page.tsx
@@ -4,6 +4,19 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div>guia</div>
+    <div>
+
+      <iframe 
+        className="scribd_iframe_embed" 
+        title="Guia de Usuario" 
+        src="https://es.scribd.com/embeds/659130775/content?start_page=1&view_mode=scroll&access_key=key-wGLA5lp74sGC3ym9eaEX" 
+        data-auto-height="false"
+        data-aspect-ratio="0.7055599060297573"
+        id="doc_90328"
+        width="100%"
+        height="600"
+      />
+    
+    </div>
   )
 }
